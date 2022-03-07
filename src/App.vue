e6f1427e18a54fa5d69c9628d84106b8797bbdb4
sophie <sophie.human.k@gmail.com>
2022-03-07 13:31:11 +0900

    Conditional Binding

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <BindingText />
   <BindingAttr />
   <BindingClass />
+  <BindingCondition />
 </template>
 
 <script>
 import BindingText from './components/BindingText';
 import BindingAttr from './components/BindingAttr';
 import BindingClass from './components/BindingClass';
+import BindingCondition from './components/BindingCondition';
 
 export default {
   name: 'App',
   components: {
     BindingText,
     BindingAttr,
-    BindingClass
+    BindingClass,
+    BindingCondition
   }
 };
 </script>
