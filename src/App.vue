7e4f005a1f37fe0efb45c5d5c078da61a7987f6e
sophie <sophie.human.k@gmail.com>
2022-03-07 10:25:57 +0900

    create a project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <BindingText />
+  <BindingStyle />
+</template>
+
+<script>
+import BindingText from './components/BindingText';
+import BindingStyle from './components/BindingStyle';
+
+export default {
+  name: 'App',
+  components: {
+    BindingText,
+    BindingStyle
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/BindingText.vue
+++ src/components/BindingText.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>1. 텍스트 바인딩 : v-text, v-html</h1>
   <!-- 1. v-text : 자바스크립트의 innerText -->
   <div>{{ user.name }}님 안녕하세요</div>
   <div v-text="user.name"></div>
@@ -10,7 +11,6 @@
   <div v-html="buttonDanger"></div>
   <!-- v-html에 코드를 삽입해서 사용할 수도 있으므로, 보안에 유의하여 반드시 신뢰할 수 있는 데이터만 바인딩해야 한다. -->
 </template>
-3
 
 <script>
 export default {
